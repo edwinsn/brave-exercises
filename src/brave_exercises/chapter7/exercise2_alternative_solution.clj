51dc63fe2ee64c92851c8564147e19983d732418
edwinsn <edwin@sprwt.io>
2023-06-24 08:50:58 -0500

    Macro for infixed syntax, WARNINGgit add src/brave_exercises/chapter7/exercise2_alternative_solution.clj! Not working yet

--- src/brave_exercises/chapter7/exercise2_alternative_solution.clj
+++ src/brave_exercises/chapter7/exercise2_alternative_solution.clj
@@ -1,4 +1,4 @@
-(ns brave-exercises.chapter7.exercise2)
+(ns brave-exercises.chapter7.exercise2-alternative-solution)
 
 ;Create an infix function that takes a list like (1 + 3 * 4 - 5)
 ;and transforms it into the lists that Clojure needs in order to correctly evaluate the expression
@@ -30,10 +30,10 @@
         left-operand (check-if-is-number-or-infixed-list left-operand-infixed)
         right-operand (check-if-is-number-or-infixed-list right-operand-infixed)
         ]
-    (let  [result (list operator
-       (if (number? left-operand) left-operand (infix left-operand))
-       (if (number? right-operand) right-operand (infix right-operand))
-       )]
+    (let  [result `((quote ~operator)
+                    ~(if (number? left-operand) left-operand `(infix ~left-operand))
+                    ~(if (number? right-operand) right-operand `(infix ~right-operand))
+                    )]
       result
       )
     )
